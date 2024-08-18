1b843260a05fe7c1365c04928c907593a3e5fe03
neurolabusc <rorden@sc.edu>
2024-08-18 17:31:35 -0400

    Use niimath with close morphology

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 ### NiiVue voxels to mesh
 
-A basic example of converting a voxel-based image to a simplified mesh.
+A basic example of converting a voxel-based image to a simplified mesh. This interactive drag-and-drop web page allows you to create meshes that can be used with a 3D printer.
+
+![Example mesh from CT scan of a human header](CTmesh.png)
 
 ### Usage
 
- - Open the [live demo](https://niivue.github.io/niivue-mesh/).
+1. Open the [live demo](https://niivue.github.io/niivue-mesh/).
+2. **Option 1** Use the `Volume` pull-down menu to select an image.
+3. **Option 2** If your image is in NIfTI format, drag and drop the file onto the web page.
+4. **Option 3** If your image is in DICOM format, use the `Select DICOM` button to import your image.
+5. Note when you click on the image, the voxel intensity is shown in the status bar at the bottom-left of the web page. You can decide a nice intensity threshold to segment your image (e.g. for a CT scan, bone will be brighter than soft tissue).
+6. Press the `Create Mesh` button and select your preferred settings: 
+![settings dialog](settings.png)
+ - The [Isosurface](https://en.wikipedia.org/wiki/Marching_cubes) is the voxel intensity used to discriminate the mesh surface. See the previous step for detials. By default, this value is set to the [Otsu threshold](https://en.wikipedia.org/wiki/Otsu%27s_method).
+ - [Closing](https://en.wikipedia.org/wiki/Closing_(morphology)) removes small crevices and cavities in your mesh which can plague printing.
+ - Fill bubbles will remove any cavities, this includes large cavities for example the ventricles for a brain scan.
+ - The `Largest cluster only` will only extract a single mesh. For example, with the `Iguana` demo this extracts only the skull without the [scleral rings](https://en.wikipedia.org/wiki/Scleral_Ring).
+ - You can choose `Smoothing` to make the surfaces less jagged (note this can create self intersecting triangles that can confound some printers).
+ - You can choose to `Simplify` [reduce the number of triangles](https://github.com/sp4cerat/Fast-Quadric-Mesh-Simplification) to create smaller files (note this can create self intersecting triangles that can confound some printers).
+7. Once you have set your preferences, press `Apply`.
+8. You will see the mesh appear and can interactively view it. If you are unhappy with the result, repeat step 6 with different settings. If you want to print the results, press the `Save Mesh` button.
 
 
 ### For Developers
--- main.js
+++ main.js
@@ -1,20 +1,4 @@
 import { Niivue, NVMeshUtilities } from '@niivue/niivue'
-// Dynamically load the worker modules
-async function createVoxelWorker() {
-  if (window.Worker) {
-    const { default: VoxelWorker } = await import('./voxelWorker?worker')
-    return new VoxelWorker()
-  }
-  throw new Error('Web Workers are not supported in this environment.')
-}
-
-async function createMeshWorker() {
-  if (window.Worker) {
-    const { default: MeshWorker } = await import('./meshWorker?worker')
-    return new MeshWorker()
-  }
-  throw new Error('Web Workers are not supported in this environment.')
-}
 
 function formatNumber(value) {
   if (Math.abs(value) >= 1) {
@@ -25,51 +9,59 @@ function formatNumber(value) {
     return value.toPrecision(3)
   }
 }
-async function main() {
-  const MeshWorker = await createMeshWorker()
-  const VoxelWorker = await createVoxelWorker()
-  const Nii2meshWorker = await new Worker('./nii2meshWorker.js')
-  const loadingCircle = document.getElementById('loadingCircle')
-  let startTime = Date.now()
-  function meshStatus(isTimed = true) {
-    let str = `Mesh has ${nv1.meshes[0].pts.length / 3} vertices and ${nv1.meshes[0].tris.length / 3} triangles`
-    if (isTimed)
-      str += ` ${Date.now() - startTime}ms`
-    document.getElementById('location').innerHTML = str
-    console.log(str)
-    shaderSelect.onchange()
+
+class NiiMathWrapper {
+  constructor(workerScript) {
+    this.worker = new Worker(workerScript)
   }
-  async function loadMz3(meshBuffer) {
-    if (nv1.meshes.length > 0) {
-      nv1.removeMesh(nv1.meshes[0])
-    }
-    await nv1.loadFromArrayBuffer(meshBuffer, 'test.mz3')
-    loadingCircle.classList.add('hidden')
-    meshStatus()
+  static async load(workerScript = './niimathWorker.js') {
+    return new NiiMathWrapper(workerScript)
   }
-  async function loadMesh(vertices, triangles) {
-    const verticesArray = new Float32Array(vertices)
-    const trianglesArray = new Uint32Array(triangles)
-    const meshBuffer = NVMeshUtilities.createMZ3(verticesArray, trianglesArray, false)
-    await loadMz3(meshBuffer)
-  }
-  MeshWorker.onmessage = async function (e) {
-    const { vertices, triangles } = e.data
-    await loadMesh(vertices, triangles)
-  }
-  VoxelWorker.onmessage = async function (e) {
-    const { vertices, triangles } = e.data
-    await loadMesh(vertices, triangles)
-  }
-  Nii2meshWorker.onmessage = async function (e) {
-    if (e.data.blob instanceof Blob) {
-        var reader = new FileReader()
-        reader.onload = () => {
-            loadMz3(reader.result)
+  niimath(niiBuffer, operationsText) {
+    return new Promise((resolve, reject) => {
+      const niiBlob = new Blob([niiBuffer], { type: 'application/octet-stream' })
+      let inName = 'input.nii' // or derive from context
+      const isMZFormat = niiBuffer.byteLength >= 2 && new Uint8Array(niiBuffer)[0] === 77 && new Uint8Array(niiBuffer)[1] === 90
+      if (isMZFormat) {
+        inName = 'input.mz3'
+      }
+      let outName = inName
+      if ((isMZFormat) || (operationsText.includes("-mesh"))) {
+        outName = 'output.mz3' // or derive from context
+      }
+      const args = operationsText.trim().split(/\s+/)
+      args.unshift(inName)
+      args.push(outName)
+      const file = new File([niiBlob], inName)
+      this.worker.onmessage = (e) => {
+        if (e.data.blob instanceof Blob) {
+          const reader = new FileReader()
+          reader.onload = () => {
+            resolve(reader.result) // return ArrayBuffer
+          }
+          reader.onerror = () => {
+            reject(new Error('Failed to read the Blob as an ArrayBuffer'))
+          }
+          reader.readAsArrayBuffer(e.data.blob)
+        } else {
+          reject(new Error('Expected Blob from worker'))
         }
-        reader.readAsArrayBuffer(e.data.blob)
-    }
+      }
+      this.worker.onerror = (e) => {
+        reject(new Error(e.message))
+      }
+      this.worker.postMessage({ blob: file, cmd: args, outName: outName })
+    })
   }
+  terminate() {
+    this.worker.terminate()
+  }
+}
+
+async function main() {
+  const wrapper = await NiiMathWrapper.load()
+  const loadingCircle = document.getElementById('loadingCircle')
+  let startTime = null
   saveBtn.onclick = function () {
     if (nv1.meshes.length < 1) {
       window.alert("No mesh open for saving. Use 'Create Mesh'.")
@@ -77,6 +69,9 @@ async function main() {
       saveDialog.show()
     }
   }
+  aboutBtn.onclick = function () {
+    window.open("https://github.com/niivue/niivue-mesh", "_blank")
+    }
   volumeSelect.onchange = function () {
     const selectedOption = volumeSelect.options[volumeSelect.selectedIndex]
     const txt = selectedOption.text
@@ -112,7 +107,10 @@ async function main() {
     if (formatSelect.selectedIndex === 2) {
       format = 'stl'
     }
-    NVMeshUtilities.saveMesh(nv1.meshes[0].pts, nv1.meshes[0].tris, `mesh.${format}`, true)
+    const pts = nv1.meshes[0].pts.slice()
+    for (let i = 0; i < pts.length; i++)
+      pts[i] *= 0.5
+    NVMeshUtilities.saveMesh(pts, nv1.meshes[0].tris, `mesh.${format}`, true)
   }
   remeshBtn.onclick = function () {
     if (nv1.volumes.length < 1) {
@@ -134,90 +132,55 @@ async function main() {
       return
     }
     startTime = Date.now()
-    const shrinkValue = Math.min(Math.max(Number(shrinkSimplePct.value) / 100, 0.01), 1)
-    if (shrinkValue >= 1)
+    let reduce = Math.min(Math.max(Number(shrinkSimplePct.value) / 100, 0.01), 1)
+    if ((reduce <= 0.0) || (reduce >= 1))
       return
     const verts = nv1.meshes[0].pts.slice()
     const tris = nv1.meshes[0].tris.slice()
+    const meshBuffer = NVMeshUtilities.createMZ3(verts, tris, false)
     loadingCircle.classList.remove('hidden')
-    if (simpleWasmCheck.checked) {
-        const meshBuffer = NVMeshUtilities.createMZ3(verts, tris, false)
-        let mz3 = new Blob([meshBuffer], {
-            type: 'application/octet-stream'
-        })
-        let inName = `em${Math.round(Math.random() * 0xffffff)}.mz3`
-        let fileMZ3 = new File([mz3], inName)
-        let outName = `em${Math.round(Math.random() * 0xffffff)}.mz3`
-          Nii2meshWorker.postMessage({
-              blob: fileMZ3,
-              percentage: shrinkValue,
-              simplify_name: outName,
-          })
-    } else {
-    MeshWorker.postMessage({
-      verts,
-      tris,
-      shrinkValue
-    })
-    }
+    let ops = " -r " + reduce.toString()
+    console.log('niimath operation', ops)
+    const arrayBuffer = await wrapper.niimath(meshBuffer, ops )
+    loadingCircle.classList.add('hidden')
+    if (nv1.meshes.length > 0)
+      nv1.removeMesh(nv1.meshes[0])
+    await nv1.loadFromArrayBuffer(arrayBuffer, 'test.mz3')
   }
   applyBtn.onclick = async function () {
-    if (nv1.volumes.length < 1) {
-      console.log('No volume open to meshify.')
-      return
-    }
     startTime = Date.now()
-    const isoValue = Number(isoNumber.value)
-    const largestCheckValue = largestCheck.checked
-    const bubbleCheckValue = bubbleCheck.checked
-    const shrinkValue = Math.min(Math.max(Number(shrinkPct.value) / 100, 0.01), 1)
-    const smoothValue = smoothSlide.value
+    const niiBuffer = await nv1.saveImage({volumeByIndex: nv1.volumes.length - 1}).buffer
     loadingCircle.classList.remove('hidden')
-    if (wasmCheck.checked) {
-      //const meshBuffer = NVMeshUtilities.createMZ3(verts, tris, false)
-      const niiBuffer = await nv1.saveImage().buffer
-      console.log('WASM nii2mesh', niiBuffer)
-      let nii = await new Blob([niiBuffer], {
-        type: 'application/octet-stream'
-      })
-      let inName = `em${Math.round(Math.random() * 0xffffff)}.nii`
-      let fileNii = await new File([nii], inName)
-      let outName = `em${Math.round(Math.random() * 0xffffff)}.mz3`
-      Nii2meshWorker.postMessage({
-          blob: fileNii,
-          percentage: shrinkValue,
-          simplify_name: outName,
-          isoValue: isoValue,
-          onlyLargest: largestCheckValue,
-          fillBubbles: bubbleCheckValue,
-          postSmooth: smoothValue
-      })
-      return
+    //mesh with specified isosurface
+    const isoValue = Number(isoNumber.value)
+    let ops = "-mesh -i " + isoValue
+    //const largestCheckValue = largestCheck.checked
+    if (!largestCheck.checked)
+      ops += " -l 0"
+    let reduce = Math.min(Math.max(Number(shrinkPct.value) / 100, 0.01), 1)
+    ops += " -r " + reduce.toString()
+    if (bubbleCheck.checked)
+      ops += " -b 1" //fill bubbles
+    let hollowSz = Number(hollowSelect.value )
+    let closeSz = Number(closeMM.value)
+    const pixDim = Math.min(Math.min(nv1.volumes[0].hdr.pixDims[1],nv1.volumes[0].hdr.pixDims[2]), nv1.volumes[0].hdr.pixDims[3])
+    if ((pixDim < 0.2) && ((hollowSz !== 0) || (closeSz !== 0))) {
+      hollowSz *= pixDim
+      closeSz *= pixDim
+      console.log('Very small pixels, scaling hollow and close values by ', pixDim)
     }
-    let img = new Float32Array(nv1.volumes[0].img)
-    const scl_slope = nv1.volumes[0].hdr.scl_slope
-    const scl_inter = nv1.volumes[0].hdr.scl_inter
-    if (scl_slope !== 1.0 || scl_inter !== 0) {
-      img = new Float32Array(img)
-      for (let i = 0; i < img.length; i++) {
-        img[i] = img[i] * scl_slope + scl_inter
-      }
+    if (hollowSz < 0)
+      ops = " -hollow 0.5 "+hollowSz + ' '+ ops
+    if ((isFinite(closeSz)) && (closeSz > 0)){
+      ops = " -close " + isoValue + " "+closeSz + ' ' + 2 * closeSz + ' '+ ops
     }
-    const dims = [nv1.volumes[0].hdr.dims[1], nv1.volumes[0].hdr.dims[2], nv1.volumes[0].hdr.dims[3]]
-    const affine = nv1.volumes[0].hdr.affine
-    VoxelWorker.postMessage(
-      {
-        img,
-        dims,
-        isoValue,
-        largestCheck: largestCheckValue,
-        bubbleCheck: bubbleCheckValue,
-        smoothValue,
-        affine,
-        shrinkValue
-      },
-      [img.buffer]
-    )
+    console.log('niimath operation', ops)
+    const arrayBuffer = await wrapper.niimath(niiBuffer, ops )
+    loadingCircle.classList.add('hidden')
+    if (nv1.meshes.length > 0)
+      nv1.removeMesh(nv1.meshes[0])
+    await nv1.loadFromArrayBuffer(arrayBuffer, 'test.mz3')
+    //nv1.reverseFaces(0)
   }
   visibleCheck.onchange = function () {
     nv1.setMeshProperty(nv1.meshes[0].id, 'visible', this.checked)
@@ -229,12 +192,18 @@ async function main() {
     nv1.setMeshShader(nv1.meshes[0].id, this.value)
   }
   function handleMeshLoaded() {
-    meshStatus(false)
+    let str = `Mesh has ${nv1.meshes[0].pts.length / 3} vertices and ${nv1.meshes[0].tris.length / 3} triangles`
+    if (startTime)
+      str += ` ${Date.now() - startTime}ms`
+    document.getElementById('location').innerHTML = str
+    console.log(str)
+    shaderSelect.onchange()
+    startTime = null
   }
   const defaults = {
     onMeshLoaded: handleMeshLoaded,
     onLocationChange: handleLocationChange,
-    backColor: [0.2, 0.2, 0.3, 1],
+    backColor: [1, 1, 1, 1],
     show3Dcrosshair: true
   }
   const nv1 = new Niivue(defaults)
@@ -256,7 +225,7 @@ async function main() {
   nv1.onImageLoaded = () => {
     imageStatus()
   }
-  nv1.setClipPlane([0.2, 0, 120])
+  nv1.setClipPlane([0.1, 0, 120])
   nv1.opts.dragMode = nv1.dragModes.pan
   nv1.setRenderAzimuthElevation(245, 15)
   nv1.opts.multiplanarForceRender = true
@@ -264,7 +233,7 @@ async function main() {
   nv1.setInterpolation(true)
   await nv1.loadVolumes([{ url: './tinyT1.nii.gz' }])
   imageStatus()
-  // applyBtn.onclick()
+  await applyBtn.onclick()
 }
 
 main()
